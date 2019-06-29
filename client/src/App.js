098368873cbf794da361576cbce569daf3dd21f2
Tin La <tinla14@gmail.com>
2019-06-29 16:46:51 -0700

    Update front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import PrivateRoute from './components/auth/PrivateRoute';
 import './App.css';
 
 // Import Components
 import Navbar from './components/navbar/navbar';
 import Login from './components/user/Login';
 import Signup from'./components/user/Signup';
+import ForgotPassword from "./components/user/PasswordReset/ForgotPassword";
+import ResetPassword from "./components/user/PasswordReset/ResetPassword";
+import Profile from './components/user/UserProfile/UserProfile';
+import EditProfile from './components/user/UserActions/EditUser';
 
 
 function App() {
@@ -16,6 +21,19 @@ function App() {
         <Switch>
           <Route exact path="/signin" component={Login} />
           <Route exact path="/signup" component={Signup} />
+          <Route exact path="/forgot-password" component={ForgotPassword} />
+          <Route
+                  exact
+                  path="/reset-password/:resetPasswordToken"
+                  component={ResetPassword}
+            />
+          {/* Private Route */}
+          <PrivateRoute exact path="/user/:userId" component={Profile} />
+          <PrivateRoute
+                    exact
+                    path="/user/edit/:userId"
+                    component={EditProfile}
+                />
         </Switch>
       </BrowserRouter>
     </div>
