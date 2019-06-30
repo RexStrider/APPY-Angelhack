3c71594c026dfd31a3b592fa6dd0985f4327241c
Zachary Owen <zach.owen50@gmail.com>
2019-06-30 09:59:20 -0700

    Calculate distance in miles using the Haversine formula.

--- client/src/components/algorithms/Geolocate.js
+++ client/src/components/algorithms/Geolocate.js
@@ -35,8 +35,11 @@ class Geolocate extends React.Component {
         ) : (
             <div>Getting the location data&hellip; </div>
         );
+        
     }
 }
+
+
 
 Geolocate.propTypes = Object.assign({}, Geolocate.propTypes, geoPropTypes);
 
@@ -46,3 +49,61 @@ export default geolocated({
     },
     userDecisionTimeout: 5000,
 })(Geolocate);
+
+
+window.setInterval(
+    function(){
+        navigator.geolocation.getCurrentPosition(display);
+    }, 1000);
+
+
+
+// https://stackoverflow.com/questions/14560999/using-the-haversine-formula-in-javascript
+Number.prototype.toRad = function() {
+    return this * Math.PI / 180;
+}
+
+let haversine = (currentLat, currentLon, initLat, initLon)=> {
+    var initLat = 42.741; 
+    var initLon = -71.3161; 
+    var currentLat = 42.806911; 
+    var currentLon = -71.290611; 
+    
+    var R = 3958.756;
+
+    var x1 = initLat-currentLat;
+    var deltaLat = x1.toRad();
+
+    var x2 = initLon-currentLon;
+    var deltaLon = x2.toRad();  
+
+    var a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) + 
+            Math.cos(currentLat.toRad()) * Math.cos(initLat.toRad()) * 
+            Math.sin(deltaLon/2) * Math.sin(deltaLon/2);
+
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+
+    return R * c;
+
+}
+
+haversine();
+
+var stationaryLatitude = 0.0;
+var stationaryLongitude = 0.0;
+var currentLatitude = 0.0;
+var currentLongitude = 0.0;
+var isStationary = false;
+
+function display(position) {
+    currentLatitude = position.coords.latitude;
+    currentLongitude = position.coords.longitude;
+
+    if (!isStationary) {
+        stationaryLatitude = currentLatitude;
+        stationaryLongitude = currentLongitude;
+        isStationary = true;
+    }
+    
+    console.log(haversine(currentLongitude, currentLongitude, currentLatitude, currentLongitude));
+}
